fc717b628292f8c81599041943c6e5b99f6b615a
Harshit Budhraja <harshitbudhraja1301@gmail.com>
2017-04-17 19:20:49 +0530

    Improved and Completed the code v2

--- Algorithms/union-find-linked.c
+++ Algorithms/union-find-linked.c
@@ -1,6 +1,7 @@
 /* To implement UNION FIND using linked list representation
 * Author:- Harshit Budhraja
-* Date:- 07/07/2017
+* Date:- 07/04/2017
+* Completed on:- 17/04/2017
 */
 
 #include<stdio.h>
@@ -12,41 +13,110 @@ struct List
 	struct Element *head;
 	struct Element *tail;
 	int size;
-}*L;
+};
 
 // The structure definition for UNION FIND data structure
 struct Element
 {
 	int value;	// The key value
-	struct Element *rep;	// Pointer to the representative of the list
+	struct List *rep;	// Pointer to the representative of the list
 	struct Element *next;	// Pointer to the immidiate next element in the list
-}*ptr;
+}
 
-struct Element *make_set(int x)
+
+// make_set() takes a value as input and creates an element and a list
+struct List *make_set(int x)
 {	
 	// Creating the element
-	ptr=(struct Element *)malloc(sizeof(struct Element));
+	struct Element ptr=(struct Element *)malloc(sizeof(struct Element));
 	ptr->value=x;
 	ptr->next=NULL;
 	ptr->rep=ptr;
 	
 	// Appending it to a list
-	L=(struct List *)malloc(sizeof(struct List));
+	struct List L=(struct List *)malloc(sizeof(struct List));
 	if(L==NULL)
 	{	
 		L->size++;
 		L->head=ptr;
 		L->tail=ptr;
 	}
-	else
-	{
-		L->size++;
-		L->tail=ptr;
-	}
-	return ptr;
+	return L;
 }
 
-// Driver function for the program
+// find_set() takes in a pointer to an element and returns the pointer to its representative element
+struct List *find_set(struct Element *x)
+{
+    return x->rep->head;
+}
+
+// union() takes input as pointers to two elements
+struct List union(struct Element *x, struct Element *y)
+{
+    // h1 and h2 are the pointers to representatives of the list containing the elements x and y
+    struct List *h1=find_set(x);
+    struct List *h2=find_set(y);
+    
+    // Now we find the smaller of the two lists and carry out the union operation appropriately
+    if(h1->size > h2->size)
+    {
+        /********************* h1 > h2 **************************/        
+        
+        // Join the smaller list behind the bigger list
+        h1->tail->next=h2->head;
+        // Update the *tail of the bigger list
+        h1->tail=h2->tail;
+        // Update the size of the bigger list
+        h1->size+=h2->size;
+        // Update the size of the smaller list
+        h2->size=0;
+        
+        // Change the representative pointers of all elements in the smaller list
+        // *ptr is an interator that iterates over the smaller list
+        struct Element *ptr = h2->head;
+        while(ptr!=NULL)
+        {
+            ptr->rep=h1;
+            ptr=ptr->next;
+        }
+        
+        // Remove the head of the smaller list
+        free(h2);
+        
+        // Return the pointer to the larger list
+        return h1;
+    }
+    else
+    {
+        /********************* h1 < h2 **************************/
+        
+        // Join the smaller list behind the bigger list
+        h2->tail->next=h1->head;
+        // Update the *tail of the bigger list
+        h2->tail=h1->tail;
+        // Update the size of the bigger list
+        h2->size+=h1->size;
+        // Update the size of the smaller list
+        h2->size=0;
+        
+        // Change the representative pointers of all elements in the smaller list
+        // *ptr is an interator that iterates over the smaller list
+        struct Element *ptr = h1->head;
+        while(ptr!=NULL)
+        {
+            ptr->rep=h2;
+            ptr=ptr->next;
+        }
+        
+        // Remove the head of the smaller list
+        free(h1);
+        
+        // Return the pointer to the larger list
+        return h2;
+    }
+}
+
+/* Driver function for the program
 int main(void)
 {
 	struct Element *e=make_set(3);
@@ -55,4 +125,4 @@ int main(void)
 	printf("%d\n",e2->value);
 	printf("%d\n",L->head->value);
 	return 0;
-}
+}*/
